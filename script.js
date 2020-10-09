f6c6b49073ad66780273e758cce738c15f584250
87penginnouta <49667189+87penginnouta@users.noreply.github.com>
2020-10-09 21:26:56 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,3 @@ var s = skrollr.init({
   smoothScrolling: false,
   forceHeight: false
 });
-
-
- var $page = document.getElementById('page'), $load = document.getElementById('loading'); // 这个定时器的作用就是模拟网页内容加载完成 setTimeout(function () { // 显示网页内容 $page.style.display = 'block'; // 隐藏或者移除加载动画 $load.style.display = 'none'; }, 3000)
