bc8b6e8319844e002a47098156cd80da0691717f
87penginnouta <49667189+87penginnouta@users.noreply.github.com>
2024-03-01 00:11:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,57 +6,57 @@ $(window).on("load", function () {
   $("#bg").css('visibility', 'visible');
 });
 
-//var s = skrollr.init({
-//smoothScrolling: false,
-//forceHeight: false
-//});
 
-var scrollObj = skrollr.init({
-  //跟smoothScrolling的功能，主要都是讓scroll事件不要這麼敏感，動畫才不會看起來卡卡的。
-  smoothScrolling: true,
-  smoothScrollingDuration: 200,
+/*-----aos-----*/
 
-  //可以定義一些常數在影格使用，Example: data-_myconst-200 and skrollr.init({constants: {myconst: 300}}) result in data-500.
+AOS.init();
 
-  constants: {
-    initTop: 100
-  },
 
-  //可以調整ScrollBar往下拉對應到keyframe的值等倍放大
-  scale: 1,
+/*-----視差-----*/
 
-  //讓文本高度自動達到滿足Keyframe的條件
-  //forceHeight: true,
+var s = skrollr.init();
 
-  //針對行動裝置的功能
-  mobileCheck: function () {},
-  mobileDeceleration: 0.004,
 
-  //畫面一開始，元素的初始值set：物件上第一個影格的值，ease：相對畫面開始的Scrolltop值使用兩格影格作參考，reset:使用他原生的CSS值
-  //edgeStrategy:'set'，
-
-  //render事件
-  render: function (data) {
-    //Log the current scroll position.
-    console.log(data.curTop);
-  }
+/*-----錨點-----*/
+$(function () {
+  $('a[href*="#"]:not([href="#"])').click(function () {
+    if (
+      location.pathname.replace(/^\//, "") ==
+      this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate({
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
 });
+
 
 /*-----返回頂端-----*/
 
-$(function() {
-    /* 按下GoTop按鈕時的事件 */
-    $('#gotop').click(function(){
-        $('html,body').animate({ scrollTop: 0 }, 'slow');   /* 返回到最頂上 */
-        return false;
-    });
-    
-    /* 偵測卷軸滑動時，往下滑超過400px就讓GoTop按鈕出現 */
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 500){
-            $('#gotop').fadeIn();
-        } else {
-            $('#gotop').fadeOut();
-        }
-    });
+$(function () {
+  /* 按下GoTop按鈕時的事件 */
+  $('#gotop').click(function () {
+    $('html,body').animate({
+      scrollTop: 0
+    }, 'slow'); 
+    return false;
+  });
+
+  /* 往下滑超過就讓GoTop按鈕出現 */
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 500) {
+      $('#gotop').fadeIn();
+    } else {
+      $('#gotop').fadeOut();
+    }
+  });
 });
